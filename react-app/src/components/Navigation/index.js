ec4c8e1d0baf6c77adf1debd477b721284c0418b
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-04-01 19:20:42 -0400

    search almost functional

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import { stocksSearch } from './SearchObject';
 
 function Navigation({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
+	const [value, setValue] = useState('')
+
+	const onChangeHandler = (e) => {
+		setValue(e.target.value)
+	}
+
+	const onClickhHandler = (company) => {
+			setValue(company)
+	}
+
+	const filterData = (data) => {
+		const filteredData = data.filter(item => {
+			const searchInfo = value.toLowerCase()
+			const companyName = item.company.toLowerCase()
+			return searchInfo && companyName.startsWith(searchInfo) && companyName !== searchInfo
+		})
+
+		const resultList = filteredData.slice(0,6)
+		return resultList
+	}
 
 	return (
 		<div className='nav-container'>
@@ -17,9 +38,18 @@ function Navigation({ isLoaded }) {
 								<img className="nav-logo-image" src='/images/robinhood-emblem.png' alt="logo" style={{ color: "black" }} />
 							</NavLink>
 						</div>
-						<div className='nav-search'>
-							<i className="fas fa-search" />
-							<input className="nav-search-bar" type="search" placeholder='Search feature coming soon' readOnly />
+						<div>
+							<div className='nav-search'>
+								<i className="fas fa-search" />
+								<input className="nav-search-bar" type="text" onChange={onChangeHandler} value={value} placeholder='Search feature coming soon' />
+							</div>
+							<div className='search-dropdown'>
+								{filterData(stocksSearch).map(item => (
+									<div key={item.company} className='search-results' onClick={() => onClickhHandler(item.company)}>
+										{item.company}
+									</div>
+								))}
+							</div>
 						</div>
 					</div>
 					<div className='nav-button-container'>
