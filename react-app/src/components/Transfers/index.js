00f577aeb6b372b264abab85d6fc3e1484c66c89
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-05-16 16:41:05 -0400

    created transfer button no functionality

--- react-app/src/components/Transfers/index.js
+++ react-app/src/components/Transfers/index.js
@@ -1,16 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { useAccountNavSelect } from '../../context/AccountNav';
+import "./transfers.css"
 
-function Transfers(){
-    const {setSelectedNav} = useAccountNavSelect()
+function Transfers() {
+    const { setSelectedNav } = useAccountNavSelect()
 
     useEffect(() => {
         setSelectedNav('transfers')
-    },[])
+    }, [])
 
     return (
-        <div>
-            Transfers
+        <div className='transfer-page-container'>
+            <div className='transfer-page-div'>
+                <div className='transfer-header-div'>
+                    <div className='transfer-header-text'>
+                        Start a transfer
+                    </div>
+                    <div className='transfer-button-div'>
+                        <button className='transfer-button-money'>
+                            <div className='transfer-icon-div'>
+                                <i className='fas fa-exchange-alt' />
+                            </div>
+                            <div className='transfer-button-text bold'>
+                                Transfer money
+                            </div>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
