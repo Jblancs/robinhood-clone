0dfecb40d3f435e750705025f582b144642746ba
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-05-22 08:54:25 -0400

    added error handling and review button

--- react-app/src/components/Transfers/TransferForm.js
+++ react-app/src/components/Transfers/TransferForm.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BankAccountForm from '../BankAccountModal/BankAccountForm';
 import "./TransferForm.css"
+import { addCommas } from '../../Utils';
 
 function TransferForm({ bank, user }) {
-    const [amount, setAmount] = useState(0)
+    const [amount, setAmount] = useState()
     const [from, setFrom] = useState("")
     const [to, setTo] = useState("")
     const [type, setType] = useState("Deposit")
+    const [confirm, setConfirm] = useState(false)
+    const [errors, setErrors] = useState([])
+    const [disableField, setDisableField] = useState(false)
 
     // Show bank account form if no bank accounts
     if (bank.error) {
@@ -20,9 +24,9 @@ function TransferForm({ bank, user }) {
     // Event Handlers ------------------------------------------------------------------------------------
     const typeFromOnChange = (e) => {
         setFrom(e.target.value)
-        if(e.target.selectedIndex === 0){
+        if (e.target.selectedIndex === 0) {
             setType("Withdrawal")
-        }else{
+        } else {
             setType("Deposit")
         }
     }
@@ -35,9 +39,99 @@ function TransferForm({ bank, user }) {
         setAmount(e.target.value)
     }
 
+    const onClickCancelHandler = () => {
+        setDisableField(false)
+        setErrors({})
+    }
+
+    const onClickEditHandler = () => {
+        setConfirm(false)
+        setDisableField(false)
+    }
+
+    const reviewHandler = () => {
+
+        let errorObj = []
+
+        if (amount > 50000) {
+            errorObj.push("Transfers may not exceed $50,000.00.")
+        }
+
+        if (amount <= 0 || !amount){
+            errorObj.push("Invalid transfer amount $0.00 provided.")
+        }
+
+        if (type === "Withdrawal" && amount > user.portfolio.buying_power){
+            errorObj.push("Withdrawal amount exceeds brokerage balance")
+        }
+
+        if (errorObj.length) {
+            setErrors(errorObj)
+        } else {
+            setConfirm(true)
+        }
+        setDisableField(true)
+    }
+
     // Submit Handler ------------------------------------------------------------------------------------
     const onSubmitHandler = (e) => {
         e.preventDefault()
+
+        const transferData = {
+            bank_account_id: (type === "Deposit" ? from : to),
+            amount: amount,
+            type: type
+        }
+    }
+
+    // Transfer Review Button ----------------------------------------------------------------------------
+    let confirmBtn;
+    if (!confirm && !errors.length) {
+        confirmBtn = (
+            <div className='review-button-div'>
+                <button className='review-button bold' onClick={reviewHandler}>
+                    Review Transfer
+                </button>
+            </div>
+        )
+    }
+
+    if (errors.length){ // show errors if any
+        confirmBtn = (
+            <div>
+                <div>
+                    <div className="error-message-div bold">
+                        <span className="info-icon error-icon bold">!</span>
+                        Error
+                    </div>
+                    <div className="error-message-div">
+                        {errors.map(message => (
+                            <div key={message} className="acct-error-message-div">&#8226; {message}</div>
+                        ))}
+                    </div>
+                </div>
+                <div className="review-button-div">
+                    {<button className="review-button bold" type="button" onClick={onClickCancelHandler}>Cancel</button>}
+                </div>
+            </div>
+        )
+    }
+
+    if (confirm) { // confirm if there are no errors
+        confirmBtn = (
+            <div>
+                <div>
+                    Order Summary
+                    <span className="info-icon bold">?</span>
+                </div>
+                <div className="review-button-div">
+                    {<button className="review-button">{`Transfer $${addCommas(amount)}`}</button>}
+                </div>
+                <div className="review-button-div">
+                    <button className="review-button edit-button" onClick={onClickEditHandler}>Cancel</button>
+                </div>
+            </div>
+        )
     }
 
     // Change to options based on transfer type ----------------------------------------------------------
@@ -74,17 +168,20 @@ function TransferForm({ bank, user }) {
                         </div>
                         <input
                             className='transfer-field'
-                            type='text'
+                            placeholder="$0.00"
+                            type="number"
+                            step="any"
                             value={amount}
                             onChange={inputOnChange}
                             required
+                            disabled={disableField}
                         />
                     </div>
                     <div className='transfer-field-div'>
                         <div className='transfer-field-text'>
                             From
                         </div>
-                        <select onChange={typeFromOnChange} defaultValue={bankAccountList[0].id}>
+                        <select onChange={typeFromOnChange} defaultValue={bankAccountList[0].id} disabled={disableField}>
                             {bankAccountList.map((bank) => (
                                 <option key={bank.id} value={bank.id}>
                                     {bank.bank} &#8226; {bank.account_type} {bank.account_number}
@@ -99,14 +196,12 @@ function TransferForm({ bank, user }) {
                         <div className='transfer-field-text'>
                             To
                         </div>
-                        <select defaultValue={type === "Deposit" ? user.portfolio.id : bankAccountList[0].id} onChange={toOnChange}>
-                                {toOptions}
+                        <select onChange={toOnChange} defaultValue={type === "Deposit" ? user.portfolio.id : bankAccountList[0].id} onChange={toOnChange} disabled={disableField}>
+                            {toOptions}
                         </select>
                     </div>
-                    {type === "Deposit" ? <div>Deposit limit: $50,000</div> : ""}
-                    <button>
-                        Review Transfer
-                    </button>
+                    {type === "Deposit" ? <div className="deposit-limit">Deposit limit: $50,000</div> : ""}
+                    {confirmBtn}
                 </form>
             </div>
         </div>
--- react-app/src/store/bankAccount.js
+++ react-app/src/store/bankAccount.js
@@ -31,7 +31,6 @@ export const fetchBankAccounts = () => async (dispatch) => {
 
     if (response.ok) {
         const data = await response.json();
-        console.log(data)
         dispatch(getBankAccounts(data));
         return data
     }
