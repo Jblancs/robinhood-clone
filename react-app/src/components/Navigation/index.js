e1be144574ce62f4cf67907ce824405ac9df862b
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-05-26 16:21:20 -0400

    added socials and fixing search bar sizing

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -9,15 +9,12 @@ function Navigation({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
 	const history = useHistory()
 	const [value, setValue] = useState('')
-	// const [stockTick, setStockTick] = useState('')
 
 	const onChangeHandler = (e) => {
 		setValue(e.target.value)
 	}
 
 	const onClickhHandler = (company, ticker) => {
-		// setValue(company)
-		// setStockTick(ticker)
 		history.push(`/stocks/${ticker}`)
 		setValue("")
 	}
@@ -44,34 +41,45 @@ function Navigation({ isLoaded }) {
 								<img className="nav-logo-image" src='/images/robinhood-emblem.png' alt="logo" style={{ color: "black" }} />
 							</NavLink>
 						</div>
-						<div>
-							<div className='nav-search'>
-								<i className="fas fa-search" />
-								<input className="nav-search-bar" type="text" onChange={onChangeHandler} value={value} placeholder='Search' />
-								<div className={value ? 'search-dropdown': "hidden"}>
-									{filterData(stocksSearch).map(item => (
-										<div key={item.company} className='search-results' onClick={() => onClickhHandler(item.company, item.ticker)}>
-											<div className='search-ticker'>{item.ticker}</div>
-											<div className='search-divider'></div>
-											<div>{item.company}</div>
-										</div>
-									))}
+						<div className='nav-search-container'>
+							<div className='nav-search-div'>
+								<div className='nav-search'>
+									<div className='nav-search-bar-div'>
+										<i className="fas fa-search" />
+										<input className="nav-search-bar" type="text" onChange={onChangeHandler} value={value} placeholder='Search' />
+									</div>
+									<div className={value ? 'search-dropdown' : "hidden"}>
+										{filterData(stocksSearch).map(item => (
+											<div key={item.company} className='search-results' onClick={() => onClickhHandler(item.company, item.ticker)}>
+												<div className='search-ticker'>{item.ticker}</div>
+												<div className='search-divider'></div>
+												<div>{item.company}</div>
+											</div>
+										))}
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-					<div className='nav-button-container'>
-						<div className={sessionUser ? 'nav-investing bold navhover' : 'nav-investing bold navhover not-allowed'}>
-							<NavLink exact to="/" style={{ textDecoration: "none", color: "black" }}>Investing</NavLink>
-						</div>
-						<div className='nav-notif bold navhover'>
-							<div className='nav-investing bold not-allowed'>Notifications</div>
-						</div>
-						{isLoaded && (
-							<div>
-								<ProfileButton user={sessionUser} />
+						<div className='nav-button-container'>
+							<div className={sessionUser ? 'nav-investing bold navhover' : 'nav-investing bold navhover not-allowed'}>
+								<NavLink exact to="/" style={{ textDecoration: "none", color: "black" }}>Investing</NavLink>
 							</div>
-						)}
+							<div className='nav-notif bold navhover'>
+								<div className='nav-investing bold' onClick={() => window.open("https://github.com/Jblancs")}>
+									Github
+								</div>
+							</div>
+							<div className='nav-notif bold navhover'>
+								<div className='nav-investing bold' onClick={() => window.open("https://www.linkedin.com/in/jordan-blancaflor/")}>
+									LinkedIn
+								</div>
+							</div>
+							{isLoaded && (
+								<div>
+									<ProfileButton user={sessionUser} />
+								</div>
+							)}
+						</div>
 					</div>
 				</>
 			) : ""}
