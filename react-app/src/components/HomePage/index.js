4b670dcbf98968d0c97c54680c69e24e5548e4bb
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-06-04 16:36:58 -0400

    added market snapshot

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -6,7 +6,7 @@ import { clearInvestmentState, fetchAllInvestments } from "../../store/investmen
 import { fetchHistory, clearHistoryState } from "../../store/portfolioHistory";
 import './HomePage.css'
 import Investments from "./Investments";
-import { getNewsArticles } from "../../Utils";
+import { addCommas, getNewsArticles, getStockData } from "../../Utils";
 import { useHistory } from "react-router-dom";
 import { clearWatchlistsState, fetchWatchlists } from "../../store/watchlist";
 import { clearBankAccountState, fetchBankAccounts } from "../../store/bankAccount";
@@ -19,6 +19,10 @@ function HomePage() {
     const dispatch = useDispatch()
     const history = useHistory()
     const [news, setNews] = useState()
+    const [priceSP500, setPriceSP500] = useState()
+    const [priceNasdaq, setPriceNasdaq] = useState()
+    const [priceBTC, setPriceBTC] = useState()
+
 
     const portfolio = useSelector(state => state.portfolio.portfolio)
     const portHistory = useSelector(state => state.history.history)
@@ -29,6 +33,9 @@ function HomePage() {
 
     useEffect(() => {
         getNewsArticles("SPY", setNews)
+        getStockData("X:BTCUSD", setPriceBTC)
+        getStockData("SPY", setPriceSP500)
+        getStockData("NDAQ", setPriceNasdaq)
     }, [])
 
     useEffect(() => {
@@ -46,11 +53,11 @@ function HomePage() {
         }
     }, [dispatch])
 
-    if(!user){
+    if (!user) {
         history.push("/login")
     }
 
-    if (!portfolio || !portHistory || !investments || !user || !news || !bank) return <div className='loading-div'><img src='/images/loading.gif' alt='loading'/></div>
+    if (!portfolio || !portHistory || !investments || !user || !news || !bank) return <div className='loading-div'><img src='/images/loading.gif' alt='loading' /></div>
 
     // Component JSX ----------------------------------------------------------------------------------------
     return (
@@ -60,8 +67,37 @@ function HomePage() {
                     <div className="portfolio-graph-div">
                         <PortfolioGraph portHistory={portHistory} portfolio={portfolio} />
                     </div>
-                    <BuyingPower portfolio={portfolio} bank={bank}/>
+                    <BuyingPower portfolio={portfolio} bank={bank} />
                     <SlideShow />
+                    <div className="portfolio-news-header bold">
+                        News
+                    </div>
+                    <div className="market-snapshot-container">
+                        <div className="market-snapshot-div">
+                            <div className="snapshot-ticker bold">
+                                SPY
+                            </div>
+                            <div className="snapshot-close">
+                                ${priceSP500 != "error" ? addCommas(Number(priceSP500.c).toFixed(2)) : "-"}
+                            </div>
+                        </div>
+                        <div className="market-snapshot-div">
+                            <div className="snapshot-ticker bold">
+                                NDAQ
+                            </div>
+                            <div className="snapshot-close">
+                                ${priceNasdaq != "error" ? addCommas(Number(priceNasdaq.c).toFixed(2)) : "-"}
+                            </div>
+                        </div>
+                        <div className="market-snapshot-div btc-div">
+                            <div className="snapshot-ticker bold">
+                                BTC
+                            </div>
+                            <div className="snapshot-close">
+                                ${priceBTC != "error" ? addCommas(Number(priceBTC.c).toFixed(2)) : "-"}
+                            </div>
+                        </div>
+                    </div>
                     <NewsCard news={news} />
                 </div>
                 <div className="stock-watchlist-component sticky">
